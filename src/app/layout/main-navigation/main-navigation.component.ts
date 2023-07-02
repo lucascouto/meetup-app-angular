cdb2c5d96f447a5315115d5b0be9ed24085b207b
Lucas Couto <loc.unb@gmail.com>
2023-07-02 02:45:38 -0300

    Routing is working!

--- src/app/layout/main-navigation/main-navigation.component.ts
+++ src/app/layout/main-navigation/main-navigation.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './main-navigation.component.html',
-  styleUrls: ['./main-navigation.component.scss']
+  styleUrls: ['./main-navigation.component.scss'],
 })
-export class MainNavigationComponent {
-
-}
+export class MainNavigationComponent {}
---
+++ src/app/pages/new-meetup/new-meetup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-meetup',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-meetup.component.html',
+  styleUrls: ['./new-meetup.component.scss']
+})
+export class NewMeetupComponent {
+
+}
